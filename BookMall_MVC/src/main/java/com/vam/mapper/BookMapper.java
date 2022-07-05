b96769a96c06368911e64fddbf9bb2357707088b
Sung <vkdnjwkrk3@naver.com>
2022-07-05 20:46:09 +0900

    0705 BookMall_MVC (1)커밋

---
+++ BookMall_MVC/src/main/java/com/vam/mapper/BookMapper.java
@@ -0,0 +1,17 @@
+package com.vam.mapper;
+
+import java.util.List;
+
+import com.vam.model.BookVO;
+import com.vam.model.Criteria;
+
+
+public interface BookMapper {
+
+	// 상품검색
+	public List<BookVO> getGoodsList(Criteria cri);
+	
+	// 상품 총 갯수
+	public int goodsGetTotal(Criteria cri);
+	
+}
